221fd7f9a30059f81d166fec232e310dc42cb76f
Pragyesh Singh Chauhan <146080551+PragyeshChauhan@users.noreply.github.com>
2025-04-23 16:10:49 +0530

    Basic Structure
    First commit

---
+++ src/main/java/com/desi/kart/desikart_backend/DesiKartBackendApplication.java
@@ -0,0 +1,13 @@
+package com.desi.kart.desikart_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesiKartBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesiKartBackendApplication.class, args);
+	}
+
+}
